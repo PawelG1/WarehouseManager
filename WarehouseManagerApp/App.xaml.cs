aaba2ae2fe81f3afc1c35788892ace0af0eb80f1
Pawel Galusza <pawelgalka153@gmail.com>
2025-12-07 21:41:00 +0100

    Initial commit: WPF Warehouse Manager Application

---
+++ WarehouseManagerApp/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace WarehouseManagerApp
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
